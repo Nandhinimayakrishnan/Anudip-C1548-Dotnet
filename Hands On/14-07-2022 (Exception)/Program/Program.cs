c1f850b2ac48857dde1555aee0f79d5277897e26
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-07-14 22:05:48 +0530

    Add files via upload

---
+++ Hands On/14-07-2022 (Exception)/Program/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            int d;
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int e;
+            try
+            {
+            Console.WriteLine("Enter value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+            c = a / b;
+            Console.WriteLine("Result is : "+c);
+            Console.WriteLine("Enter a integer value: ");
+            d = Int32.Parse(Console.ReadLine());
+            e = arr[6];
+            Console.WriteLine(" Value of e is: "+e);
+           }
+           catch(DivideByZeroException obj)
+          {
+           Console.WriteLine( obj.Message);
+          }
+          catch(FormatException)
+          {
+           Console.WriteLine("Enter only integer values");
+          }
+          catch( IndexOutOfRangeException obj)
+          {
+           Console.WriteLine(obj.Message);
+          }
+          finally
+          {
+           Console.WriteLine(" THE PROGRAM IS END ");
+          }
+
+        }
+    }
+}
