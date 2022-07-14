6465ed0009a97a09b699a6c35ce5b7aa051dc2bc
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-07-14 17:43:24 +0530

    Add files via upload

---
+++ Lab Section/Lab Programs/14.07.2022 - Programs/Program (1).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Write a program to find the factorial of a number 
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, fact;
+            Console.WriteLine(" Enter The Number: ");
+            number = Int32.Parse(Console.ReadLine());
+            fact = number;
+            for (i = number - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial Of Given Number Is : "+fact);
+            
+
+        }
+    }
+}
