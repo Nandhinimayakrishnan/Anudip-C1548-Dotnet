25a5fdb905dae109f7472b6f0dc1ca037327059f
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-07-29 07:49:56 +0530

    Add files via upload

---
+++ Hands On/31-05-2022 (Method overloading)/Program/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication21
+{
+    interface i1
+    {
+        void get();
+    }
+    interface i2
+    {
+        void print();
+    }
+    class person
+    {
+        public void show()
+        {
+            Console.WriteLine("Just to show message");
+        }
+    }
+    class sample : person, i1, i2
+    {
+        int a;
+        public void get()
+        {
+            a = 20;
+        }
+        public void print()
+        {
+            Console.WriteLine(a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sample obj = new sample();
+            obj.show();
+            obj.get();
+            obj.print();
+        }
+    }
+}
