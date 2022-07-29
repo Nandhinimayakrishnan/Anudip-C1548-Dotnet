f8df8a48db9a9b965448c8a2ba5b36d270d7682c
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-07-29 07:48:49 +0530

    Add files via upload

---
+++ Hands On/29-05-2022 Interface/Interface Program/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Write a program using two interfaces.
+namespace ConsoleApplication21
+{
+    interface i1
+    {
+        void get();
+    }
+    interface i2
+    {
+        void print();
+    }
+    class person
+    {
+        public void show()
+        {
+            Console.WriteLine("Just to show message");
+        }
+    }
+    class sample : person, i1, i2
+    {
+        int a;
+        public void get()
+        {
+            a = 20;
+        }
+        public void print()
+        {
+            Console.WriteLine(a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sample obj = new sample();
+            obj.show();
+            obj.get();
+            obj.print();
+        }
+    }
+}
