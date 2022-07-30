4f4092bce6c893f7be7c0ac7c34c8cad09e3d831
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-07-30 16:27:50 +0530

    Add files via upload

---
+++ Hands On/5-7-2022(Delegate&Event)/Program/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    //Write a program Delegate&Event(Without Arguments).
+    class Sample
+    {
+        int no;
+        public delegate void mydelegate();
+        public event mydelegate Myevent;
+        public void Input()
+        {
+            for(int i=1;i<=5;i++)
+            {
+                Console.WriteLine("Enter integer:");
+                no = Int32.Parse(Console.ReadLine());
+                if (no < 0)
+                    Myevent();
+                else
+                    Console.WriteLine("Perfect");
+               
+            }
+            // void checknumber()
+           // {
+           //    Console.WriteLine("You have entered negative value:");
+           // }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample obj = new Sample();
+           // obj.Myevent += new Sample.mydelegate(obj.checknumber);
+            obj.Input();
+        }
+    }
+}
