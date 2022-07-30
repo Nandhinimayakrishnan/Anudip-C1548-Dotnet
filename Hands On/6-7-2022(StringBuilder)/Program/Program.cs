cd725256a49abf1ef0e740d7a7294b65e6a89d33
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-07-30 16:28:23 +0530

    Add files via upload

---
+++ Hands On/6-7-2022(StringBuilder)/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder S = new StringBuilder("Siva");
+            Console.WriteLine(S);
+            S.Append("Karthikeyan");
+            Console.WriteLine("After adding text at the end" +S);
+            Console.WriteLine("No of characters in a string" +S.Length);
+            //string.S= S.ToString();
+            //Console.WriteLine("From string builder" +S1);
+
+        }
+    }
+}
