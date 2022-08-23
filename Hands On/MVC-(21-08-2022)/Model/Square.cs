8b8665a21fd6466e1a00d65b5b514eacba91fdef
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-08-23 13:17:12 +0530

    Add files via upload

---
+++ Hands On/MVC-(21-08-2022)/Model/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Areaofsquare.Models
+{
+    public class Square
+    {
+        public int side { get; set; }
+
+      //  public int side { get; set; }
+
+        public int result { get; set; }
+    }
+}
