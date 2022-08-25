255315f4c255c9aa4041fa93e26fd030c5a642fd
Nandhinimayakrishnan <105638669+Nandhinimayakrishnan@users.noreply.github.com>
2022-08-25 17:58:10 +0530

    Add files via upload

---
+++ Lab Section/Lab Programs/MVC-(25-08-2022)/Controller/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication23.Models;
+
+namespace WebApplication23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Arithmetic Calc)
+        {
+            if(Calc.Calculation == "Addition")
+            {
+                Calc.Result = Calc.Firstnumber + Calc.Secondnumber;
+            }
+
+            if (Calc.Calculation == "Subtraction")
+            {
+                Calc.Result = Calc.Firstnumber - Calc.Secondnumber;
+            }
+            if (Calc.Calculation == "Multiplication")
+            {
+                Calc.Result = Calc.Firstnumber * Calc.Secondnumber;
+            }
+            if (Calc.Calculation == "Division")
+            {
+                Calc.Result = Calc.Firstnumber + Calc.Secondnumber;
+            }
+
+
+            return View(Calc);
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab Section/Lab Programs/MVC-(25-08-2022)/Model/Arithmetic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication23.Models
+{
+    public class Arithmetic
+    {
+        public int Firstnumber { get; set; }
+        public int Secondnumber { get; set; }
+        public int Result { get; set; }
+        public String Calculation { get; set; }
+    }
+}
